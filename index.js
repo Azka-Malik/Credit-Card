84d02dacbabeb4dd2e6401160ec03302df88035e
Azka-Malik <azkamalik309@gmail.com>
2022-11-04 02:12:03 +0500

    Done

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const prompt = require("prompt-sync")({ sigint: true });
 var number='';
 number= (prompt("Number : "));
 i=0;
-n=0;
-dEven=false;
-nCheck=0;
+numberOdd='';
+numberEven='';
+odds=0;
+evens=0;
+sumOdd=0;
+sumEven=0;
+TotalSum=0;
 
 if(number.length==15){
     console.log("American Express");
@@ -17,25 +21,37 @@ else if(number.length==13 || number.length==16){
     console.log("Visa")
 }
 
-for (i=number.length-1;i>=0;i--)
-{
-    var digit=number.charAt(i);
-    n = parseInt(digit, 10);
-    
-    //console.log(nDigit);
-    if (dEven) {
-        if ((n *= 2) > 9) n -= 9;
+for(i=0;i<number.length;i += 2){
+    odds = number[i] * 2;
+    if(odds>9){
+        odds -= 9;
+    }
+    numberOdd += odds;
+    sumOdd += odds;
+}
+//console.log(numberOdd);
+//console.log(sumOdd);
+
+for(i=1;i<number.length;i+= 2){
+    evens = parseInt(number[i]);
+    numberEven += evens;
+    sumEven += evens;
+}
+//console.log(numberEven);
+//console.log(sumEven);
+TotalSum = sumOdd + sumEven;
+console.log(TotalSum);
+if((TotalSum % 10) == 0){
+        console.log("Valid Number")
+    }
+    else{
+        console.log("Invalid Number")
     }
 
-    nCheck += n;
-    dEven = !dEven;
-}
-//console.log(nCheck);
 
-if((nCheck % 10) == 0){
-    console.log("Valid Number")
-}
-else{
-    console.log("Invalid Number")
-}
+
+
+
+
+
 
